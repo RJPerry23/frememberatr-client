b057f4c86134eeb767536b5dd4f2c68e508bbc00
Robert Perry <101219279+RJPerry23@users.noreply.github.com>
2022-05-11 17:15:58 -0400

    Add login page and begin work on sign-in page. Not much functionality built in yet.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import './styles/App.scss';
+import { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage'
+import SignUpPage from './pages/SignUpPage/SignUpPage'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={LoginPage}/>
+          <Route path="/signup" component={SignUpPage}/>
+        </Switch>
+        </BrowserRouter>
+      </div>
+    );  
+  }
 }
 
 export default App;
---
+++ src/pages/SignUpPage/SignUpPage.js
@@ -0,0 +1,59 @@
+import './SignUpPage.scss'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/logos/Frememberatr_logo.png'
+
+class SignUpPage extends Component {
+
+    hidePlaceHolder = (e) => {
+        e.target.placeholder = ""
+    }
+
+    resetForm = () => {
+        const form = document.getElementById("login")
+        form.reset()
+    }
+
+    handleLogin = (e) => {
+        e.preventDefault()
+    }
+
+    render() {
+        return (
+            <div className='login' onClick={this.resetForm}>
+                <img src={Logo} className='login__logo'/>
+                <form id='login' name='login' className='login__form'>
+                    <h1 className='login__form--heading'>"Friend - remember - ator"</h1>
+                    <input className='login__form--username'
+                    type='text'
+                    placeholder='Please enter a username'
+                    htmlFor='login'
+                    name='username'
+                    id='username'
+                    onClick={this.hidePlaceHolder}>
+                    </input>
+                    <input className='login__form--password'
+                    type='text'
+                    placeholder='Please enter a password'
+                    htmlFor='login'
+                    name='password'
+                    id='password'
+                    onClick={this.hidePlaceHolder}>
+                    </input>
+                    <input className='login__form--submit'
+                    type='submit'
+                    value='SUBMIT'
+                    onSubmit={this.handleLogin}>
+                    </input>
+                </form>
+                <p className='login__sign-up'>Don't have an account?</p>
+                <Link to="/signup" className='link'>
+                <p className='login__sign-up--link'>Click here to sign up.</p>
+                </Link>
+            </div>
+        );
+    
+    }
+};
+
+export default SignUpPage;
